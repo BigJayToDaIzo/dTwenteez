cf05f22b044e99e91332a42f2b676bc7f2fb18d4
Joseph Myers <josephlmyers@gmail.com>
2025-03-08 11:25:04 -0600

    init commit d20 mvp

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // MVP in notebook
+    // 1)Ask user for #d20 to roll
+    // 2)Give user quit option
+    // 3)Handle input errors gracefully
+    // 4)display roll total, and individual rolls in brackets
+    // 5)80% code coverage in test framework
+    //
+    // Task 1: Lay scaffold for testing
+    // Task 2: Display greeting
+    // Task 3: Capture user input
+    // Task 4: display results of roll
+    // Task 5: Ask for next roll/quit
+    println!("d20 DN!");
+}
