a9e61882503ff51674c7c883398130348cbdd3e7
Joseph Myers <josephlmyers@gmail.com>
2025-03-09 22:55:29 -0500

    roll configuration abstracted into library, roll on deck

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,89 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+pub struct Config {
+    pub h_opt: bool,
+    pub h_flag: bool,
+    pub advantage: bool,
+    pub disadvantage: bool,
+    pub sides: u32,
+    pub count: u32,
+    pub modifier: i32,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Config {
+    pub fn new() -> Config {
+        Config {
+            h_opt: false,
+            h_flag: false,
+            advantage: false,
+            disadvantage: false,
+            sides: 20,
+            count: 1,
+            modifier: 0,
+        }
+    }
+    pub fn build<T>(&mut self, args: &mut T)
+    where
+        T: Iterator<Item = String>,
+    {
+        args.next(); //discard application path
+        while let Some(arg) = args.next() {
+            match arg.as_str() {
+                "-h" => self.h_opt = true,
+                "--help" => self.h_flag = true,
+                "-a" => {
+                    self.advantage = true;
+                    self.disadvantage = false;
+                }
+                "-d" => {
+                    self.advantage = false;
+                    self.disadvantage = true;
+                }
+                "-s" => {
+                    if let Some(arg) = args.next() {
+                        if let Ok(sides) = arg.parse::<u32>() {
+                            self.sides = sides;
+                        }
+                    }
+                }
+                "-c" => {
+                    if let Some(arg) = args.next() {
+                        if let Ok(count) = arg.parse::<u32>() {
+                            self.count = count;
+                        }
+                    }
+                }
+                "-m" => {
+                    if let Some(arg) = args.next() {
+                        if let Ok(modifier) = arg.parse::<i32>() {
+                            self.modifier = modifier;
+                        }
+                    }
+                }
+                x => {
+                    println!("arg string broken with invalid argument: {x}");
+                    self.h_opt = true;
+                }
+            }
+        }
+        if self.h_opt || self.h_flag {
+            // this will get much larger and need to be abstracted
+            println!(
+                "usage: d20 [-h | --help | -a | -d | -c N | -s N | -m +|-N]
+** example d20 -a -c 1 -s 20 -m +9
+** Astarion rolls with advantage, one d20 with a +9 modifier"
+            );
+        }
+    }
+    // pub fn roll(&self) -> Roll {
+    //     // how do we build the roll?
+    //     // lets start with a single d20 roll no modifiers
+    // }
 }
 
 #[cfg(test)]
@@ -7,8 +91,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn new_works() {
+        let c = Config::default();
+        assert_eq!(c.sides, 20);
+        assert_eq!(c.count, 1);
+        assert_eq!(c.modifier, 0);
     }
+    #[test]
+    fn build_works() {
+        let mut c = Config::default();
+        let mut args = ["", "-h", "-d", "-m", "-2"].iter().map(|s| s.to_string());
+        c.build(&mut args);
+        assert!(c.h_opt);
+        assert!(c.disadvantage);
+        assert_eq!(c.modifier, -2);
+    }
+    // #[test]
+    // fn roll_single_d20_no_modifiers() {
+    //     let mut c = Config::default();
+    //     let roll = c.roll();
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,16 @@
+use d20::Config;
 use std::env;
-// We must isolate ONLY these 4 tasks to main, all else should be abstracted
-// It's ok to build it here, but it must be abstracted when main becomes large
-// argument values (if arg parsing isn't also
-//    abstracted)
 
+// We must isolate ONLY these 4 tasks to main, all else should be abstracted
 fn main() {
-    println!("Welcome to best dn roller of all tiem!1!");
-    println!("That's not a deez nutz joke, n represents a the range [2/4/8/10/20/100]");
+    println!("We hope you enjoy the best dicer: Rollin' @DN");
+    println!("That's not a deez nutz joke, N represents the range [2/4/8/10/20/100]");
     // 1) Calling the command line parsing logic with
     let mut args = env::args();
-    let mut c = Config::new(); // default config to build
-    args.next(); //discard application path
-    // 2) Setting up configuration
-    while let Some(arg) = args.next() {
-        match arg.as_str() {
-            "-h" => c.h_opt = true,
-            "--help" => c.h_flag = true,
-            "-a" => {
-                c.advantage = true;
-                c.disadvantage = false;
-            }
-            "-d" => {
-                c.advantage = false;
-                c.disadvantage = true;
-            }
-            "-s" => {
-                if let Some(arg) = args.next() {
-                    if let Ok(sides) = arg.parse::<u32>() {
-                        c.sides = sides;
-                    }
-                }
-            }
-
-            "-c" => {
-                if let Some(arg) = args.next() {
-                    if let Ok(count) = arg.parse::<u32>() {
-                        c.count = count;
-                    }
-                }
-            }
-            "-m" => {
-                if let Some(arg) = args.next() {
-                    if let Ok(modifier) = arg.parse::<i32>() {
-                        c.modifier = modifier;
-                    }
-                }
-            }
-            x => {
-                println!("arg string broken with invalid argument: {x}");
-                c.h_opt = true;
-            }
-        }
-    }
-    if c.h_opt || c.h_flag {
-        // this will get much larger and need to be abstracted
-        println!(
-            "usage: d20 [-h | --help | -a | -d | -c N | -s N | -m +|-N]
-** example d20 -a -c 1 -s 20 -m +9
-** Astarion rolls with advantage, one d20 with a +9 modifier"
-        );
-    }
-    dbg!(&c);
+    let mut c = Config::default(); // default config to build
+    // 2) Setting up configuration (ABSTRACT THIS NAO)
+    c.build(&mut args);
+    // let roll = c.roll();
     // 3) Calling a run function in lib.rs
     // 4) Handling errors if run returns error
-}
-
-#[derive(Debug)]
-struct Config {
-    h_opt: bool,
-    h_flag: bool,
-    advantage: bool,
-    disadvantage: bool,
-    sides: u32,
-    count: u32,
-    modifier: i32,
-}
-
-impl Config {
-    fn new() -> Config {
-        Config {
-            h_opt: false,
-            h_flag: false,
-            advantage: false,
-            disadvantage: false,
-            sides: 20,
-            count: 1,
-            modifier: 0,
-        }
-    }
 }
