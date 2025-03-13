44bf6a0d78deca578f4b1513f95a5eecb959dac6
Joseph Myers <josephlmyers@gmail.com>
2025-03-13 10:32:38 -0500

    roll tests and struct refactor

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,28 @@ use roll::Roll;
 use std::env;
 
 // We must isolate ONLY these 4 tasks to main, all else should be abstracted
+// 1) Calling the command line parsing logic with
+// 2) Setting up configuration (ABSTRACT THIS NAO)
+// 3) Calling a run function in lib.rs
+// 4) Handling errors if run returns error
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("We hope you enjoy the best dicer: Rollin' @DN");
-    println!("That's not a deez nutz joke, N represents the range [2/4/8/10/20/100]");
-    // 1) Calling the command line parsing logic with
     let mut args = env::args();
-    let mut c = Config::default(); // default config to build
-    // 2) Setting up configuration (ABSTRACT THIS NAO)
-    c.build(&mut args);
-    let r = Roll::new(&c);
-    dbg!(&r.final_roll);
-    dbg!(&r.display);
-    // 3) Calling a run function in lib.rs
-    // 4) Handling errors if run returns error
+    let mut c = Config::default();
+    match args.len() {
+        1 => {
+            // show help and give default d20 roll
+            c.h_opt = true;
+            let roll = Roll::new(&c);
+            let log = roll.roll_log;
+            let roll = roll.final_roll;
+            let out = format!("{log} = {roll}");
+            println!("{out}");
+        }
+        2 => {
+            c.build(&mut args); // only display help
+        }
+        _ => {}
+    }
 }
--- src/roll.rs
+++ src/roll.rs
@@ -4,14 +4,13 @@ use rand::Rng;
 #[derive(Debug)]
 pub struct Roll {
     pub final_roll: u32,
-    pub display: String,
+    pub roll_log: String,
 }
 
 impl Roll {
     pub fn new(c: &Config) -> Roll {
         let mut rng = rand::rng();
         // this will evolve heavily
-        // build display
         let mut display: String = String::from("[ ");
         let mut rt_roll = 0;
         if c.advantage {
@@ -40,7 +39,6 @@ impl Roll {
                 display.push_str(&string_appendage);
             }
         }
-        // has modifier?
         match c.modifier {
             i32::MIN..0 => {
                 let modifier = c.modifier;
@@ -50,23 +48,22 @@ impl Roll {
                 } else {
                     rt_roll += modifier;
                 }
-                let final_string_appendage = format!("] mod: {modifier} = {rt_roll}");
+                let final_string_appendage = format!("] mod: {modifier}");
                 display.push_str(&final_string_appendage);
             }
             0 => {
-                let final_string_appendage = format!("] = {rt_roll}");
-                display.push_str(&final_string_appendage);
+                display.push(']');
             }
             1..=i32::MAX => {
                 let modifier = c.modifier;
                 rt_roll += modifier;
-                let final_string_appendage = format!("] mod: +{modifier} = {rt_roll}");
+                let final_string_appendage = format!("] mod: +{modifier}");
                 display.push_str(&final_string_appendage);
             }
         }
         Roll {
             final_roll: rt_roll as u32,
-            display,
+            roll_log: display,
         }
     }
 }
@@ -96,64 +93,54 @@ mod test {
         assert!((1..=c.sides).contains(&roll.final_roll));
     }
     #[test]
-    fn d2_no_modifiers_display() {
+    fn d2_no_modifiers_log() {
         // ex: [ 2 ] = 2
         let c = build_config(vec!["", "-s", "2"]);
         let roll = Roll::new(&c);
-        let regex = Regex::new(r"\[ (?<summation_detail>\d) \] = (?<total>\d)").unwrap();
-        let Some(cap) = regex.captures(&roll.display) else {
+        let regex = Regex::new(r"\[ (?<summation_detail>\d) \]").unwrap();
+        let Some(cap) = regex.captures(&roll.roll_log) else {
             panic!("display did not capture value");
         };
         assert_eq!(c.sides, 2);
-        assert_eq!(cap.len(), 3); // cap[0] is entire haystack
+        assert_eq!(cap.len(), 2); // cap[0] is entire haystack
         assert!((1..=c.sides).contains(&cap["summation_detail"].parse::<u32>().unwrap()));
-        assert!((1..=c.sides).contains(&cap["total"].parse::<u32>().unwrap()));
-        assert_eq!(&cap["summation_detail"], &cap["total"]);
     }
     #[test]
-    fn d20_no_modifiers_display() {
+    fn d20_no_modifiers_log() {
         let c = Config::default();
         let roll = Roll::new(&c);
-        let regex = Regex::new(r"\[ (?<summation_detail>\d{1,2}) \] = (?<total>\d{1,2})").unwrap();
-        let Some(cap) = regex.captures(&roll.display) else {
+        let regex = Regex::new(r"\[ (?<summation_detail>\d{1,2}) \]").unwrap();
+        let Some(cap) = regex.captures(&roll.roll_log) else {
             panic!("display did not capture values");
         };
-        assert_eq!(cap.len(), 3); // cap[0] is entire haystack
+        assert_eq!(cap.len(), 2); // cap[0] is entire haystack
         assert!((1..=c.sides).contains(&cap["summation_detail"].parse::<u32>().unwrap()));
-        assert!((1..=c.sides).contains(&cap["total"].parse::<u32>().unwrap()));
-        assert_eq!(&cap["summation_detail"], &cap["total"]);
     }
     #[test]
-    fn d20_pos_modifier_display() {
+    fn d20_pos_modifier_log() {
         // [ 10 ] mod: +2 = 12
         let c = build_config(vec!["", "-m", "2"]);
         let roll = Roll::new(&c);
-        let regex =
-            Regex::new(r"\[ (?<roll>\d{1,2}) \] mod: (?<modifier>\+\d{1,2}) = (?<total>\d{1,2})")
-                .unwrap();
-        let Some(cap) = regex.captures(&roll.display) else {
+        let regex = Regex::new(r"\[ (?<roll>\d{1,2}) \] mod: (?<modifier>\+\d{1,2})").unwrap();
+        let Some(cap) = regex.captures(&roll.roll_log) else {
             panic!("display didn't capture values")
         };
-        assert_eq!(cap.len(), 4);
+        assert_eq!(cap.len(), 3);
         assert!((1..=c.sides).contains(&cap["roll"].parse::<u32>().unwrap()));
         assert_eq!(cap["modifier"].parse::<u32>().unwrap(), 2);
-        assert!((1..=(c.sides + 2)).contains(&cap["total"].parse::<u32>().unwrap()));
     }
     #[test]
-    fn d20_neg_modifier_display() {
+    fn d20_neg_modifier_log() {
         // [ 10 ] mod: -2 = 8
         let c = build_config(vec!["", "-m", "-2"]);
         let roll = Roll::new(&c);
-        let regex =
-            Regex::new(r"\[ (?<roll>\d{1,2}) \] mod: (?<modifier>\-\d{1,2}) = (?<total>\d{1,2})")
-                .unwrap();
-        let Some(cap) = regex.captures(&roll.display) else {
+        let regex = Regex::new(r"\[ (?<roll>\d{1,2}) \] mod: (?<modifier>\-\d{1,2})").unwrap();
+        let Some(cap) = regex.captures(&roll.roll_log) else {
             panic!("display didn't capture values")
         };
-        assert_eq!(cap.len(), 4);
+        assert_eq!(cap.len(), 3);
         assert!((1..=c.sides).contains(&cap["roll"].parse::<u32>().unwrap()));
         assert_eq!(cap["modifier"].parse::<i32>().unwrap(), -2);
-        assert!((1..=(c.sides - 2)).contains(&cap["total"].parse::<u32>().unwrap()));
     }
     #[test]
     fn neg_modifier_does_not_lower_roll_below_one() {
@@ -163,33 +150,31 @@ mod test {
         assert_eq!(roll.final_roll, 1);
     }
     #[test]
-    fn advantage_display_works() {
+    fn advantage_log_works() {
         // [ [1,2] max: 2 ] = 2
         let c = build_config(vec!["", "-a"]);
         let roll = Roll::new(&c);
-        let regex = Regex::new(
-            r"\[ \[(?<roll1>\d{1,2}),(?<roll2>\d{1,2})\] max: (?<max>\d{1,2}) \] = (?<final>\d{1,2})",
-        )
-        .unwrap();
-        let Some(cap) = regex.captures(&roll.display) else {
+        let regex =
+            Regex::new(r"\[ \[(?<roll1>\d{1,2}),(?<roll2>\d{1,2})\] max: (?<max>\d{1,2}) \]")
+                .unwrap();
+        let Some(cap) = regex.captures(&roll.roll_log) else {
             panic!("display didn't capture values")
         };
-        assert_eq!(cap.len(), 5);
-        assert_eq!(cap["max"], cap["final"]);
+        assert_eq!(cap.len(), 4);
+        assert!(cap["max"] >= cap["roll1"] || cap["max"] >= cap["roll2"]);
     }
     #[test]
     fn disadvantage_display_works() {
         // [ [1,2] max: 2 ] = 2
         let c = build_config(vec!["", "-d"]);
         let roll = Roll::new(&c);
-        let regex = Regex::new(
-            r"\[ \[(?<roll1>\d{1,2}),(?<roll2>\d{1,2})\] min: (?<min>\d{1,2}) \] = (?<final>\d{1,2})",
-        )
-        .unwrap();
-        let Some(cap) = regex.captures(&roll.display) else {
+        let regex =
+            Regex::new(r"\[ \[(?<roll1>\d{1,2}),(?<roll2>\d{1,2})\] min: (?<min>\d{1,2}) \]")
+                .unwrap();
+        let Some(cap) = regex.captures(&roll.roll_log) else {
             panic!("display didn't capture values")
         };
-        assert_eq!(cap.len(), 5);
-        assert_eq!(cap["min"], cap["final"]);
+        assert_eq!(cap.len(), 4);
+        assert!(cap["min"] <= cap["roll1"] && cap["min"] <= cap["roll2"]);
     }
 }
